bc9a5fd0e138c5212c255b8e0213d917fb4911ac
Lynxgsm <aili.fida.ac@gmail.com>
2022-03-21 11:49:01 +0300

    done with placing react tailwind and vite

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <h1 className="font-bold text-4xl">HomePage</h1>
+    </div>
+  );
+};
+
+export default HomePage;
